d76c13be07f18cc904abca4ea727ae1ac604588b
alexis-anderson <aanderson9313@gmail.com>
2019-11-19 08:27:58 -0500

    starting mvp outline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 
+
 function App() {
+
   return (
     <div className="App">
       <div>
         <img className = "pic" src ="" alt ="nasa photo fo the day"></img> 
         <p>lorem ipsum here</p>
-    
+        
       </div>
-
+ 
     <div className = "text">
       <p>more text</p>
     </div>
