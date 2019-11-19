cbfa327c8d7b616b37fbb2854bf125c9348bd05e
alexis-anderson <aanderson9313@gmail.com>
2019-11-19 08:35:42 -0500

    outline done for photocard

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,3 +1,28 @@
 import React from 'react';
 
+const PhotoCard = () => {
+    return (
+        <div  className = "Card_container">
+            <div className = "photo_container">
+                <h1 className = "title">
+
+                </h1>
+                <img src = "" alt ="Nasa Photo Of The Day"></img>
+                <h2 className = "date">
+
+                </h2>
+                
+
+            </div>
+
+            <div className = "info">
+                <h3 className = "caption">
+                </h3>
+                <p></p>
+            </div>
+           
+        </div>
+    );
+};
+
 export default PhotoCard;
