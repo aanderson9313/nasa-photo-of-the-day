1613f5f832d3d7f2e461c31c59c519966692c49a
alexis-anderson <aanderson9313@gmail.com>
2019-11-21 22:33:08 -0500

    added logo

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -4,9 +4,10 @@ import styled from "styled-components";
 
 const NasaCard = styled.div `
 text-align: center;
-background: darkblue;
+background: black;
 color: white;
 `;
+
 const NasaImg = styled.img `
 display: flex;
 align-content: center;
@@ -14,32 +15,44 @@ justify-content: center;
 height: 20%;
 width: 50%;
 border: 2px dashed red;
-border-radius: 45%;
+border-radius: 50%;
+margin-left: 25%;
 `;
 
+const NasaExp = styled.div `
+display: flex;
+flex-direction: column;
+color: white;
+`;
 
 
 
 const PhotoCard = (props) => {
     console.log(NasaCard);
     return (
-        <NasaCard>
-            <h1>
-                {props.title}
-            </h1>
-            <h2>
-                {props.date}
-            </h2>
-            <NasaImg src = {props.url} alt = "NASA POTD"/>
-        <NasaCard>
-            <h3>
-                Explanation
-            </h3>
-            <p>
-                {props.explanation}
-            </p>
-        </NasaCard>
-        </NasaCard>
+
+            <NasaCard>
+                <div>
+                    <header>
+                        <img src = "https://api.nasa.gov/assets/footer/img/favicon-192.png" alt = "nasa logo" />
+                    </header>
+                </div>
+                <h1>
+                    {props.title}
+                </h1>
+                <h2>
+                    {props.date}
+                </h2>
+                <NasaImg src = {props.url} alt = "NASA POTD"/>
+                <NasaExp>
+                    <h3>
+                        Explanation
+                    </h3>
+                    <p>
+                        {props.explanation}
+                    </p>
+                </NasaExp>
+         </NasaCard>
     );
 };
 
