9c243192f9cdc4461983a413412a79d7ee097859
alexis-anderson <aanderson9313@gmail.com>
2019-11-21 22:00:17 -0500

    adding styled-components

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,26 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+
+
+const NasaCard = styled.div `
+text-align: center;
+background: darkblue;
+color: white;
+`;
+const NasaImg = styled.img `
+display: flex;
+align-content: center;
+justify-content: center;
+height: 20%;
+width: 50%;
+border: 2px dashed red;
+border-radius: 45%;
+`;
+
+
 
 
 const PhotoCard = (props) => {
-    
+    console.log(NasaCard);
     return (
-        <div>
+        <NasaCard>
             <h1>
                 {props.title}
             </h1>
             <h2>
                 {props.date}
             </h2>
-            <img src = {props.url} alt = "NASA POTD"/>
-        <div>
+            <NasaImg src = {props.url} alt = "NASA POTD"/>
+        <NasaCard>
             <h3>
                 Explanation
             </h3>
             <p>
                 {props.explanation}
             </p>
-        </div>
-        </div>
+        </NasaCard>
+        </NasaCard>
     );
 };
 
